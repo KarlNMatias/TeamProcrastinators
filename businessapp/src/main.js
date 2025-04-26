0b009ffd35a7adb15e05ddd825eb25b9e38282cf
KarlNMatias <147355500+KarlNMatias@users.noreply.github.com>
2025-04-25 18:33:08 -1000

    Delete businessapp directory

--- businessapp/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-
-const app = createApp(App); // Define `app` before using it
-app.use(router);
-app.mount('#app');
-
